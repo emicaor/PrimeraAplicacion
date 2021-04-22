03dc0bfc62ffb938317fe1b168cd99bf111e07e5
Alumno_T <Alumno_T@PC-00>
2021-04-22 17:51:58 +0200

    Creación de la clase de prueba llamada CalculadoraTest por Emilio Casado Ortega

---
+++ test/calculadora/CalculadoraTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Alumno_T
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Calculadora.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Calculadora.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of suma method, of class Calculadora.
+     */
+    @Test
+    public void testSuma() {
+        System.out.println("suma");
+        double num1 = 0.0;
+        double num2 = 0.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 0.0;
+        double result = instance.suma(num1, num2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of resta method, of class Calculadora.
+     */
+    @Test
+    public void testResta() {
+        System.out.println("resta");
+        double num1 = 0.0;
+        double num2 = 0.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 0.0;
+        double result = instance.resta(num1, num2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiplication method, of class Calculadora.
+     */
+    @Test
+    public void testMultiplication() {
+        System.out.println("multiplication");
+        double num1 = 0.0;
+        double num2 = 0.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 0.0;
+        double result = instance.multiplication(num1, num2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of division method, of class Calculadora.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        double num1 = 0.0;
+        double num2 = 0.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 0.0;
+        double result = instance.division(num1, num2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
